81a6ee33efe4f0510b75d221362da4b1fc4462c4
zilonghe <495309675@qq.com>
2016-09-18 00:48:41 +0800

    Return client's request info

---
+++ 01-echo-request-info/server.py
@@ -0,0 +1,46 @@
+import BaseHTTPServer
+
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """handler request and return html page"""
+
+    Page = '''\
+    <html>
+    <body>
+    <table>
+    <tr>  <td>Header</td>         <td>Value</td>          </tr>
+    <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+    <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+    <tr>  <td>Client port</td>    <td>{client_port}</td> </tr>
+    <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+    <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+    </table>
+    </body>
+    </html>
+    '''
+
+    def do_GET(self):
+        self.send_content(self.create_page())
+
+    def create_page(self):
+        values = {
+                'date_time': self.date_time_string(),
+                'client_host': self.client_address[0],
+                'client_port': self.client_address[1],
+                'command': self.command,
+                'path': self.path
+            }
+        page = self.Page.format(**values)
+        return page
+
+    def send_content(self, page):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(page)))
+        self.end_headers()
+        self.wfile.write(page)
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
